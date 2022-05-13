fe24547b9d9ca58a5dd08b99e4edefb85b49ec3b
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-05-13 13:04:33 -0500

    Create 1_Combine_organism(R1+R2).py

---
+++ 2_Combine_GC_bins/1_Combine_organism(R1+R2).py
@@ -0,0 +1,264 @@
+import shutil
+# ---------------------------PART A: Bin Files ---------------------------
+# Merging sequences below each other
+with open('R(0-5)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 0-5.txt', 'Z_WTW_S2_R2_Range 0-5.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(0-5)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(0-5)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(5-10)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 5-10.txt', 'Z_WTW_S2_R2_Range 5-10.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(5-10)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(5-10)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(10-15)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 10-15.txt', 'Z_WTW_S2_R2_Range 10-15.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(10-15)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(10-15)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(15-20)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 15-20.txt', 'Z_WTW_S2_R2_Range 15-20.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(15-20)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(15-20)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(20-25)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 20-25.txt', 'Z_WTW_S2_R2_Range 20-25.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(20-25)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(20-25)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(25-30)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 25-30.txt', 'Z_WTW_S2_R2_Range 25-30.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(25-30)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(25-30)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(30-35)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 30-35.txt', 'Z_WTW_S2_R2_Range 30-35.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(30-35)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(30-35)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(35-40)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 35-40.txt', 'Z_WTW_S2_R2_Range 35-40.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(35-40)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(35-40)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(40-45)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 40-45.txt', 'Z_WTW_S2_R2_Range 40-45.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(40-45)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(40-45)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(45-50)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 45-50.txt', 'Z_WTW_S2_R2_Range 45-50.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(45-50)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(45-50)Z_WTW_S2.txt","w")
+f.write(output)
+
+
+
+with open('R(50-55)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 50-55.txt', 'Z_WTW_S2_R2_Range 50-55.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(50-55)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(50-55)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(55-60)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 55-60.txt', 'Z_WTW_S2_R2_Range 55-60.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(55-60)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(55-60)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(60-65)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 60-65.txt', 'Z_WTW_S2_R2_Range 60-65.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(60-65)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(60-65)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(65-70)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 65-70.txt', 'Z_WTW_S2_R2_Range 65-70.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(65-70)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(65-70)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(70-75)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 70-75.txt', 'Z_WTW_S2_R2_Range 70-75.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(70-75)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(70-75)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(75-80)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 75-80.txt', 'Z_WTW_S2_R2_Range 75-80.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(75-80)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(75-80)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(80-85)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 80-85.txt', 'Z_WTW_S2_R2_Range 80-85.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(80-85)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(80-85)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(85-90)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 85-90.txt', 'Z_WTW_S2_R2_Range 85-90.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(85-90)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(85-90)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(90-95)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 90-95.txt', 'Z_WTW_S2_R2_Range 90-95.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(90-95)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(90-95)Z_WTW_S2.txt","w")
+f.write(output)
+
+with open('R(95-100)Z_WTW_S2.txt', 'w') as wfd:
+    for f in ['Z_WTW_S2_R1_Range 95-100.txt', 'Z_WTW_S2_R2_Range 95-100.txt']:
+        with open(f, 'r') as fd:
+            shutil.copyfileobj(fd, wfd)
+            wfd.write("\n")
+output=""
+with open('R(95-100)Z_WTW_S2.txt') as f:
+    for line in f:
+        if not line.isspace():
+            output+=line
+f = open("R(95-100)Z_WTW_S2.txt","w")
+f.write(output)
