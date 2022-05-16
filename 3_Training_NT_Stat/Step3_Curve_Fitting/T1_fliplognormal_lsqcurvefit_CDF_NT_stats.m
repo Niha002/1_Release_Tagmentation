91a802e2507360e560cddc5bf6dcedb5887bc046
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-05-16 15:13:12 -0500

    Update T1_fliplognormal_lsqcurvefit_CDF_NT_stats.m

--- 3_Training_NT_Stat/Step3_Curve_Fitting/T1_fliplognormal_lsqcurvefit_CDF_NT_stats.m
+++ 3_Training_NT_Stat/Step3_Curve_Fitting/T1_fliplognormal_lsqcurvefit_CDF_NT_stats.m
@@ -1,4 +1,9 @@
 clc
+
+load('T0_final.mat')
+load('Histogram_xdata.mat')
+load('Histogram_ydata.mat')
+
 NT = ['A','C','G','T'];
 FileName = input('Enter sequence-file name: ','s');
 Seq = readcell(FileName);
@@ -70,22 +75,21 @@ if ismember (resp,['Y','y'])
     x0 = [1.0, 0.5, -0.5];
     x1 = [2.5, 0.09, 10.0];
     % lognormal is flipped as -ve tail on the other end
-    w_fun = fliplognorm(x1, score);
+    w_fun = T0_func_fliplognorm(x1, score);
     % f = integral(@(s) fliplognorm (x1,s), -Inf, Inf) %integral of initial estimate
 
     % LSQCURVIT on the initial flip lognormal values
     options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt');
     lb = []; ub = [];
-    q = lsqcurvefit(@fliplognorm,x1,xdata,ydata,lb,ub,options)
-    h = integral(@(s) fliplognorm(q,s),-Inf, Inf)
-    q_fun = fliplognorm(q,score);
+    q = lsqcurvefit(@T0_func_fliplognorm,x1,xdata,ydata,lb,ub,options);
+    h = integral(@(s) T0_func_fliplognorm(q,s),-Inf, Inf);
+    q_fun = T0_func_fliplognorm(q,score);
 
     % CDF intrgrating PDF
     for j = 1:length(score)
-        rk(j) = integral(@(s) fliplognorm(q,s),-Inf, score(j))
+        rk(j) = integral(@(s) T0_func_fliplognorm(q,s),-Inf, score(j));
     end
 
-    % plot functions
     Fig1 = figure('Name','Motif-score PDF','NumberTitle','off');
     title('Motif-score Probability Density and Cumulative Density');
     xlabel('Score Bins');
@@ -95,12 +99,12 @@ if ismember (resp,['Y','y'])
     % Score
     bar(xdata,ydata, 'DisplayName','Scores');
     % PDF
-    yyaxis left
+    yyaxis left;
     plot(score,q_fun,'b','LineWidth',2.0, 'DisplayName','PDF');
     ylabel('PDF');
     % CDF
-    yyaxis right
-    gd = plot(score,rk, 'r','LineWidth',2.0, 'DisplayName','CDF')
+    yyaxis right;
+    gd = plot(score,rk, 'r','LineWidth',2.0, 'DisplayName','CDF');
     ylabel('CDF');
     hold off;
     legend
@@ -108,7 +112,7 @@ if ismember (resp,['Y','y'])
     % Lookup table (x,y) = (score,cdf)
     XData = get(gd, 'XData');
     YData = get(gd, 'YData');
-    LookupTable = [XData' YData']
+    LookupTable = [XData' YData'];
     
 end
 
