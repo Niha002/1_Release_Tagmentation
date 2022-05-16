a346e05858c081e2cdbd5df051659d299c3ad782
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-05-16 15:26:19 -0500

    Update T2_Only_CDF_LookupTable.m

--- 3_Training_NT_Stat/Step3_Curve_Fitting/T2_Only_CDF_LookupTable.m
+++ 3_Training_NT_Stat/Step3_Curve_Fitting/T2_Only_CDF_LookupTable.m
@@ -1,9 +1,10 @@
 clc; clear all;
 
-load('hist');
+load('T0_Histogram.mat');
+
 binw = xdata(2)-xdata(1);
 sumhist = 0;
-for i=1:length(xdata)
+for i=1:length(xdata);
     sumhist = sumhist + binw*ydata(i); 
 end
 disp(sumhist);
@@ -14,19 +15,19 @@ score = linspace(xdata(1), xdata(end)); % x-axis for plotting
 x0 = [1.0, 0.5, -0.5];
 x1 = [2.5, 0.09, 10.0];
 % lognormal is flipped as -ve tail on the other end
-w_fun = fliplognorm(x1, score);
+w_fun = T0_func_fliplognorm(x1, score);
 % f = integral(@(s) fliplognorm (x1,s), -Inf, Inf) %integral of initial estimate
 
 % LSQCURVIT on the initial flip lognormal values
 options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt');
 lb = []; ub = [];
-q = lsqcurvefit(@fliplognorm,x1,xdata,ydata,lb,ub,options)
-h = integral(@(s) fliplognorm(q,s),-Inf, Inf)
-q_fun = fliplognorm(q,score);
+q = lsqcurvefit(@T0_func_fliplognorm,x1,xdata,ydata,lb,ub,options);
+h = integral(@(s) T0_func_fliplognorm(q,s),-Inf, Inf);
+q_fun = T0_func_fliplognorm(q,score);
 
 % CDF intrgrating PDF
-for j = 1:length(score)
-    rk(j) = integral(@(s) fliplognorm(q,s),-Inf, score(j))
+for j = 1:length(score);
+    rk(j) = integral(@(s) T0_func_fliplognorm(q,s),-Inf, score(j));
 end
 
 % plot functions
@@ -36,22 +37,22 @@ xlabel('Score Bins');
 ylabel('Number of scores');
 
 hold on;
-set(gca,'FontSize',30)
+set(gca,'FontSize',30);
 % Score
 bar(xdata,ydata, 'DisplayName','Scores');
 % PDF
-yyaxis left
+yyaxis left;
 plot(score,q_fun,'b','LineWidth',2.0, 'DisplayName','PDF');
 ylabel('PDF');
 % CDF
-yyaxis right
-gd = plot(score,rk, 'r','LineWidth',2.0, 'DisplayName','CDF')
+yyaxis right;
+gd = plot(score,rk, 'r','LineWidth',2.0, 'DisplayName','CDF');
 ylabel('CDF');
 
 hold off;
-legend
+legend;
 
 % Lookup table (x,y) = (score,cdf)
 XData = get(gd, 'XData');
 YData = get(gd, 'YData');
-LookupTable = [XData' YData']
+LookupTable = [XData' YData'];
