fd1f2f1058d7fac490d0f5f0a5f6724edf01b44e
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-06-23 12:11:58 -0500

    Update e)_Only_T2T_automated.m

--- Other_supplement/e)_Only_T2T_automated.m
+++ Other_supplement/e)_Only_T2T_automated.m
@@ -1,6 +1,7 @@
 % Input files that Roverscan outputs for T2T chromosomes
 % Output: Plot nu vs coordinates for T2T
 
+% Have the outputfiles for all chromosomes in same directory run the code to get output for all 24 chromosomes.
 numlist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 for i = 1: length(numlist)
     % Change n to chromosome number OR x, y in '' as 'Y'
