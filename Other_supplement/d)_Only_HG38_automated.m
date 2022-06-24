ab13d3c48b991582c5a4322da20627899f0262d4
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-06-24 14:44:04 -0500

    Update d)_Only_HG38_automated.m

--- Other_supplement/d)_Only_HG38_automated.m
+++ Other_supplement/d)_Only_HG38_automated.m
@@ -2,8 +2,7 @@
 % Output: Plot nu vs coordinates for Hg38
 
 % Have the outputfiles for all chromosomes in same directory run the code to get output for all 24 chromosomes.
-numlist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-for i = 1: length(numlist) % iterate the array
+for i = 1: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];% iterate the array
     n = 0
     
     % For X chromosome 23 and Y chromsosme 24
