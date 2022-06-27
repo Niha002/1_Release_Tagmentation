54d58d826fa0459e0201a8b198c14f1195dd0e59
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-06-27 14:57:51 -0500

    Update c)_Compare_nu(Hg38_vs_T2T).m

--- Other_supplement/c)_Compare_nu(Hg38_vs_T2T).m
+++ Other_supplement/c)_Compare_nu(Hg38_vs_T2T).m
@@ -1,54 +1,56 @@
 % Input of two files that Roverscan outputs for Hg38 and T2T for the same chromosome
 % Output: Plot nu vs coordinates for both assemblies on the same graph 
 
-% Change n to chromosome number
-n = 'Y'; 
+for i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];% iterate the array
+    n = 0;
+    
+    % For X chromosome 23 and Y chromsosme 24
+    if i == 23;
+        n = 'X';
+    elseif i == 24;
+        n = 'Y';
+    else
+        n = i;
+    end
 
-% Coordinates and nu values for both Hg38 assembly and T2T 
-OutputFile1 = readmatrix("OutputFile_"+n+".txt"); 
-OutputFileT2T1 = readmatrix("OutputFile_T2T_"+n+".txt"); 
+    OutputFile1 = readmatrix("OutputFile_"+n+".txt"); 
+    OutputFileT2T1 = readmatrix("OutputFile_T2T_Chr"+n+".txt"); 
 
-% nu
-nu = OutputFile1(:,[3]); 
-nu_T2T = OutputFileT2T1(:,[3]);
-% Coordinates
-Coords = OutputFile1(:,[1]);
-Coords_T2T = OutputFileT2T1(:,[1]);
+    nu = OutputFile1(:,[3]); 
+    Coords = OutputFile1(:,[1]);
 
-% Hg38 +- 1std 
-diff = std(nu);
-p1 = mean(nu) - diff;
-p2 = mean(nu) + diff;
-% T2T +- 1std 
-diff_t2t = std(nu_T2T);
-p1_t2t = mean(nu_T2T) - diff_t2t;
-p2_t2t = mean(nu_T2T) + diff_t2t;
+    diff = std(nu);
+    p1 = mean(nu) - diff;
+    p2 = mean(nu) + diff;
 
-% Plot
-set(gca,'FontSize',35)
-set(gcf, 'WindowState', 'maximized') % fullscreen
-title("Chromosome " + n + ",window of 25000bp"); 
-    ylabel('Nu');
-    xlabel('Coordinates in bp');
+    set(gca,'FontSize',35)
+    set(gcf, 'WindowState', 'maximized')
+    title("Chromosome " + n + ",window of 25000bp"); 
+        ylabel('Nu');
+        xlabel('Coordinates in bp');
+    hold on
+    plot(Coords(:,[1]),nu,'LineWidth',1.5,'DisplayName','Nu');
+    %yline(mean(nu),'r-','Mean','LineWidth',3.0, 'DisplayName','Mean') ;
 
-hold on
-% Hg38
-plot(Coords(:,[1]),nu,'LineWidth',1.5,'DisplayName','Nu');
-yline(mean(nu),'r-','Mean','LineWidth',3.0, 'DisplayName','Mean');
-% T2T 
-plot(Coords_T2T(:,[1]),nu_T2T,'LineWidth',1.5,'DisplayName','Nu');
-yline(mean(nu_T2T),'r-','Mean','LineWidth',3.0, 'DisplayName','Mean');
+    nu_T2T = OutputFileT2T1(:,[3]);
+    Coords_T2T = OutputFileT2T1(:,[1]);
 
-% 1 std deviation
-% Hg38
-yline(p1_t2t,':','DisplayName','-σ');
-yline(p2_t2t,':','DisplayName','+σ');
-% T2T
-yline(p1,'k--', 'DisplayName','-σ');
-yline(p2,'k--','DisplayName','+σ');
+    diff_t2t = std(nu_T2T);
+    p1_t2t = mean(nu_T2T) - diff_t2t;
+    p2_t2t = mean(nu_T2T) + diff_t2t;
 
-hold off
-legend("Chr "+ n,'Mean',"T2T Chr "+ n)
+    plot(Coords_T2T(:,[1]),nu_T2T,'LineWidth',1.5,'DisplayName','Nu');
+    %yline(mean(nu_T2T),'r-','Mean','LineWidth',3.0, 'DisplayName','Mean') ;
+    ylim([0.04,0.1])
+    yline(p1_t2t,':','DisplayName','-σ');
+    yline(p2_t2t,':','DisplayName','+σ');
 
-% Save image
-saveas(gcf,"Compare_Chr"+n+".jpg")
+    yline(p1,'k--', 'DisplayName','-σ');
+    yline(p2,'k--','DisplayName','+σ');
+
+    hold off
+    legend("Chr "+ n,"T2T Chr "+ n)
+    saveas(gcf,"Compare_Chr"+n+".jpg")
+
+    close
+end 
