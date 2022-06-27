bb7f7aeee79e0f04f54d0940b7df6e90f75b521b
Niharika Pandala <55808380+Niha002@users.noreply.github.com>
2022-06-27 11:41:29 -0500

    Update and rename e)_Only_T2T_automated.m to e)_Only_T2T_Nu_automated.m

--- Other_supplement/e)_Only_T2T_Nu_automated.m
+++ Other_supplement/e)_Only_T2T_Nu_automated.m
@@ -2,7 +2,7 @@
 % Output: Plot nu vs coordinates for T2T
 
 % Have the outputfiles for all chromosomes in same directory run the code to get output for all 24 chromosomes.
-for i = 1: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];% iterate the array
+for i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];% iterate the array
     n = 0
     
     % For X chromosome 23 and Y chromsosme 24
